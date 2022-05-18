0517d25d13d0c7c2cd329fe18fe882c87e72c8bf
attiasa22 <Ariel.E.Attias.22@dartmouth.edu>
2022-05-18 12:02:00 -0400

    adding react

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # harp_ai
-COSC 89.11 Watson Project
+# COSC 89.11 Watson Project
+
+## Ariel Attias, Pat Howard, James Verschleiser
+
+### Frontend: React.js
+### Backend: Firebase
+
+
+### To run project:
+in terminal run: 
+cd harp-ai
+npm start  
---
+++ harp-ai/src/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA765RoPk0ObQbeN4JegQ2kcBQPXBfNufM",
+  authDomain: "harp-ai.firebaseapp.com",
+  projectId: "harp-ai",
+  storageBucket: "harp-ai.appspot.com",
+  messagingSenderId: "123448523556",
+  appId: "1:123448523556:web:6b6829d597ba26664051b1",
+  measurementId: "G-6CSX1FERSM"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
