fd181d2fdbf1b6c5355aed36498794c9031f80b9
attiasa22 <Ariel.E.Attias.22@dartmouth.edu>
2022-05-27 11:58:51 -0400

    added firebase and react frameworks

--- src/components/partyAdmin.js
+++ src/components/partyAdmin.js
@@ -5,14 +5,29 @@ class PartyAdmin extends Component {
       super(props);
   
       this.state = {};
+  
     }
 
     render() {
         return (
             <div className="home-page">
+                <script>        
+                    function onButtonClick(emotion){
+                      print("Hello")
+                      
+                    }
+                </script>
                 <h1 id="main-title">Your Party</h1>
                 <h2>Participants:</h2>
                 <button type="button">Record</button>
+                <h2>Does this song make you feel too:</h2>
+                <div className='.button-align'>               
+                    <input type="submit" class=" emotion-button joy-button" value="" />
+                    <input type="submit" class="sad-button emotion-button" value="" />
+                    <input type="submit" class="angry-button emotion-button" value="" />
+                    <input type="submit" class="disgusted-button emotion-button" value="" />
+                    <input onclick="onButtonClick('confused');" type="submit" class="confused-button emotion-button" value="" />
+                </div>
             </div>
         );
     }
