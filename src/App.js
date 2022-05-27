fc43474c94ac7c85f1ac365a4d4d4729244e03ac
Patrick Howard <patrick.t.howard.23@dartmouth.edu>
2022-05-27 09:41:24 -0400

    She works

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
-// Audio: https://wecode101.com/audio-recoding-and-playback-in-react-js
-
 import React, { Component } from 'react';
-import Audio from "./components/audio.component";
 import {
   BrowserRouter,
   Routes,
@@ -13,149 +10,12 @@ import Home from './components/home';
 import JoinParty from './components/joinParty';
 import PartyAdmin from './components/partyAdmin';
 
-const Mp3Recorder = new MicRecorder({ bitRate: 128 });
-
 class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-
-      isRecording: false,
-
-      blobURL: '',
-
-      isBlocked: false,
-
-      isRecordingStp: false,
-
-    };
-
-    // this.start = this.start.bind(this); //this makes the website blank for some reason
-
-    // this.stop = this.stop.bind(this);
-
-    // this.reset = this.reset.bind(this);
-
+    this.state = {};
   }
-
-  componentDidMount(){
-
-    //Prompt the user for permission to allow audio device in browser
-
-    navigator.getUserMedia = (
-
-      navigator.getUserMedia ||
-
-      navigator.webkitGetUserMedia ||
-
-      navigator.mozGetUserMedia ||
-
-      navigator.msGetUserMedia
-
-    );
-
-       //Detects the action on user click to allow or deny permission of audio device
-
-       navigator.getUserMedia({ audio: true },
-
-        () => {
-  
-          console.log('Permission Granted');
-  
-          this.setState({ isBlocked: false });
-  
-        },
-  
-        () => {
-  
-          console.log('Permission Denied');
-  
-          this.setState({ isBlocked: true })
-  
-        },
-  
-      );
-  
-    }
-
-    start(){
-
-      /*  
-  
-       * If the user denys permission to use the audio device
-  
-       * in the browser no recording can be done and an alert is shown
-  
-       * If the user allows permission the recoding will begin
-  
-       */
-  
-      if (this.state.isBlocked) {
-  
-        alert('Permission Denied');
-  
-      } else {
-  
-        Mp3Recorder
-  
-          .start()
-  
-          .then(() => {
-  
-            this.setState({ isRecording: true });
-  
-          }).catch((e) => console.error(e));
-  
-      }
-  
-    }
-
-    stop() {
-
-      /*
- 
-      * Once the recoding starts the stop button is activated
- 
-      * Click stop once recording as finished  
- 
-      * An MP3 is generated for the user to download the audio
- 
-      */
- 
-     Mp3Recorder
- 
-       .stop()
- 
-       .getMp3()
- 
-       .then(([buffer, blob]) => {
- 
-         const blobURL = URL.createObjectURL(blob)
- 
-         this.setState({ blobURL, isRecording: false });
- 
-         this.setState({ isRecordingStp: true });
- 
-       }).catch((e) => console.log(e));
- 
-   };
-
-   reset() {
-
-    /*    
-
-     * The user can reset the audio recording  
-
-     * once the stop button is clicked    
-
-     */
-
-    document.getElementsByTagName('audio')[0].src = '';
-
-    this.setState({ isRecordingStp: false });
-
-};
 
   render() {
     return (
--- src/components/partyAdmin.js
+++ src/components/partyAdmin.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import Audio from './audio.component';
+import MicRecorder from 'mic-recorder-to-mp3';
+const Mp3Recorder = new MicRecorder({ bitRate: 128 });
 
 class PartyAdmin extends Component {
     constructor(props) {
@@ -12,7 +15,7 @@ class PartyAdmin extends Component {
             <div className="home-page">
                 <h1 id="main-title">Your Party</h1>
                 <h2>Participants:</h2>
-                <button type="button">Record</button>
+                <Audio/>
             </div>
         );
     }
